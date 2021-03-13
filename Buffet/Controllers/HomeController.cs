263b2b29a517be37a06f903f5735fa541a61412b
Saulo_Calixto <saulo.santos@viannasempre.com.br>
2021-03-12 21:02:19 -0300

    Estabelecimento do projeto

---
+++ Buffet/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Buffet.Models;
+using Buffet.Models.Buffet.Cliente;
+using Buffet.ViewModels.Home;
+
+namespace Buffet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        
+        public IActionResult Clientes()
+        {
+            //trazer lista de Entidades(Entity) de clientes do service de clientes (model)
+            var clienteService = new ClienteService();
+            var listaDeClientes = clienteService.ObterClientes();
+            
+            //criar e popular viewModel
+            var viewModel = new ClientesViewModel();
+
+            foreach (ClienteEntity clienteEntity in listaDeClientes)
+            {
+                viewModel.Clientes.Add(new Cliente
+                {
+                    Nome = clienteEntity.Nome,
+                    DataDeNascimento = clienteEntity.DataDeNascimento.ToShortDateString()
+                });
+            }
+            
+            return View(viewModel);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
---
+++ Buffet/Models/Buffet/Cliente/ClienteService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Buffet.Models.Buffet.Cliente
+{
+    public class ClienteService
+    {
+        public List<ClienteEntity> ObterClientes()
+        {
+            var listaDeClientes = new List<ClienteEntity>();
+
+            listaDeClientes.Add(new ClienteEntity
+            {
+                Id = 1,
+                Nome = "saulo",
+                DataDeNascimento = new DateTime(2001,03,13)
+            });
+            
+            listaDeClientes.Add(new ClienteEntity
+            {
+                Id = 2,
+                Nome = "pedro",
+                DataDeNascimento = new DateTime(2002,04,14)
+            });
+            
+            //serviço retornando entidades(Entity) de clientes
+            return listaDeClientes;
+        }
+    }
+}
