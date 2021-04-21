4ef0034dc00461421b360952a218cb94e25dca38
Saulo_Calixto <saulo.santos@viannasempre.com.br>
2021-04-21 20:23:00 -0300

    Commit contendo a modificação da questão 5 da APS.

---
+++ Buffet/Models/Buffet/Convidado/ConvidadoEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Buffet.Models.Buffet.Evento;
+using Buffet.Models.Buffet.Situacao;
+
+namespace Buffet.Models.Buffet.Convidado
+{
+    public class ConvidadoEntity
+    {
+        
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+        public EventoEntity Evento { get; set; }
+        public SituacaoConvidadoEntity Situacao { get; set; }
+        public string TextoObservacao { get; set; }
+        public DateTime DataInsercao { get; set; }
+        public DateTime DataUltimaModificacao { get; set; }
+        
+        
+    }
+}
