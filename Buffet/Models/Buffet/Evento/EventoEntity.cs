8a0edde50862f84ba00ec19cf8e1429f4537138d
Saulo_Calixto <saulo.santos@viannasempre.com.br>
2021-04-21 20:11:20 -0300

    Commit contendo a modificação da questão 4 da APS.

--- Buffet/Models/Buffet/Evento/EventoEntity.cs
+++ Buffet/Models/Buffet/Evento/EventoEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Buffet.Models.Buffet.Cliente;
+using Buffet.Models.Buffet.Situacao;
 
 namespace Buffet.Models.Buffet.Evento
 {
@@ -8,6 +9,23 @@ namespace Buffet.Models.Buffet.Evento
         public int Id { get; set; }
         public string Descricao { get; set; }
 
+        public string Tipo { get; set; }
+
+        public DateTime DataHoraInicio { get; set; }
+
+        public DateTime DataHoraTermino { get; set; }
+
+        public ClienteEntity Cliente { get; set; }
+
+        public SituacaoEventoEntity Situacao { get; set; }
+
+        public string Local { get; set; }
+
+        public string TextoObservacao { get; set; }
+
+        public DateTime DataInsercao { get; set; }
+
+        public DateTime DataUltimaModificacao { get; set; }
         public EventoEntity(int id, string descricao)
         {
             Id = id;
