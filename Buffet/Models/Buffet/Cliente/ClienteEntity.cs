bc823ca7b6bd80d95ec7e5f7ba7425d60001cf19
Saulo_Calixto <saulo.santos@viannasempre.com.br>
2021-04-21 20:04:40 -0300

    Commit contendo a modificação da questão 3 da APS.

--- Buffet/Models/Buffet/Cliente/ClienteEntity.cs
+++ Buffet/Models/Buffet/Cliente/ClienteEntity.cs
@@ -8,9 +8,27 @@ namespace Buffet.Models.Buffet.Cliente
     public class ClienteEntity
     {
         public int Id { get; set; }
-
         public string Descricao { get; set; }
 
+        public TipoClienteEntity TipoCliente { get; set; }
+
+        public string Cpf { get; set; }
+
+        public DateTime DataDeNascimento { get; set; }
+
+        public string Cnpj { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string TextoObservacao { get; set; }
+
+        public DateTime DataInsercao { get; set; }
+
+        public DateTime DataUltimaModificacao { get; set; }
         public ClienteEntity(int id, string descricao)
         {
             Id = id;
---
+++ Buffet/Models/Buffet/Cliente/TipoClienteEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Buffet.Models.Buffet.Cliente
+{
+    public class TipoClienteEntity
+    {
+        public string Tipo { get; set; } 
+    }
+}
